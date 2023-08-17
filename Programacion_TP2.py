6038d59a4271410ae6a2c94597e1c9f4b72a8778
Renchu109 <141747231+Renchu109@users.noreply.github.com>
2023-08-17 08:51:55 -0300

    Add files via upload

---
+++ Programacion_TP2.py
@@ -0,0 +1,90 @@
+
+#Ejercicio 1
+
+numero1 = 32
+
+#Ejercicio 2
+
+numero2 = 0.7
+
+#Ejercicio 3
+
+suma = numero1 + numero2
+print(suma)
+
+#Ejercicio 4
+
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print(resta, multiplicacion, division)
+
+#Ejercicio5
+
+nombre = "Renzito"
+print(nombre)
+
+#Ejercicio 6 
+
+precio = 19.99
+print(precio)
+
+#Ejercicio 7
+
+descuento = 0.5
+
+#Ejercicio 8
+
+precio_final = precio * descuento
+print(precio_final)
+
+#Ejercicio 9
+
+cadena = "Argentina campeon del mundo 2026"
+print(cadena)
+
+#Ejercicio 10
+
+longitud = len(cadena)
+print(longitud)
+
+#Ejercicio 11
+
+precio = 5.8
+print(precio)
+precio_entero = int(precio)
+print(precio_entero)
+
+#Ejercicio 12
+
+nombre = "Rodo"
+apellido = "ariza"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+
+#Ejercicio 13
+
+edad = 19
+print(edad)
+edad = edad + 5
+print(edad)
+edad = edad - 10
+print(edad)
+
+#Ejercicio 14
+
+altura = 1.7
+print(altura)
+altura = altura * 4
+print(altura)
+altura = altura / 3
+print(altura)
+
+#Ejercicio 15
+
+nombre_mayus = "RENZO"
+print(nombre_mayus.lower())
+
+#Ejercicio 16
+nombre_mayus = "RENZO"
+print(nombre_mayus.capitalize())
